b06226f43889cd765b69349384578b27db348088
FurmanVlad <vladfurman98@gmail.com>
2024-07-14 19:36:25 +0300

    Added test for Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -18,7 +18,7 @@ function Navbar() {
   };
 
   return (
-    <nav className="navbar">
+    <nav className="navbar"  data-testid="navbar">
       <ul className="navbar-list">
         <li className="navbar-item">
           <Link to="/" className="navbar-link">Home</Link>
