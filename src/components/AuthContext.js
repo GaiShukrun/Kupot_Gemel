98d5ba610464a21a38391a5050d961cb0f511a1a
Gai <gaish@ac.sce.ac.il>
2024-08-04 12:29:10 +0300

    Passing the id to the FavoriteFunds component to fetch all the funds via user id BSPMS2434-61

--- src/components/AuthContext.js
+++ src/components/AuthContext.js
@@ -5,6 +5,8 @@ const AuthContext = createContext();
 const AuthProvider = ({ children }) => {
     const [isAuthenticated, setIsAuthenticated] = useState(false);
     const [userRole, setUserRole] = useState(null);
+    const [userId, setUserId] = useState(null);
+
 
     useEffect(() => {
         const token = localStorage.getItem('token');
@@ -15,6 +17,8 @@ const AuthProvider = ({ children }) => {
                 setIsAuthenticated(true);
                 const payload = JSON.parse(atob(token.split('.')[1]));
                 setUserRole(payload.role);
+                setUserId(payload.userId); // Add this line to set the userId
+
             } catch (error) {
                 console.error('Error parsing token:', error);
                 logout(); // Clear authentication if token parsing fails
@@ -26,16 +30,20 @@ const AuthProvider = ({ children }) => {
         localStorage.setItem('token', token);
         setIsAuthenticated(true);
         setUserRole(role);
+        setUserId(null); // Set the userId when logging in
+
     };
 
     const logout = () => {
         localStorage.removeItem('token');
         setIsAuthenticated(false);
         setUserRole(null);
+        setUserId(null); // Clear the userId when logging out
+
     };
 
     return (
-        <AuthContext.Provider value={{ isAuthenticated, userRole, login, logout }}>
+        <AuthContext.Provider value={{ isAuthenticated, userRole,userId, login, logout }}>
             {children}
         </AuthContext.Provider>
     );
