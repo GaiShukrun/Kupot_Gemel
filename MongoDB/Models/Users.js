e135c9277806821b0a21897b5fd3634ff2037979
Gai <gaish@ac.sce.ac.il>
2024-08-04 12:28:28 +0300

    Added a New sub model , the accumilated funds, and added a new field "FavoriteFunds" to the use model BSPMS2434-61

--- MongoDB/Models/Users.js
+++ MongoDB/Models/Users.js
@@ -1,5 +1,33 @@
 const mongoose = require('mongoose');
 
+const fundAnalyticsSchema = new mongoose.Schema({
+    fundId: String,
+    fundName: String,
+    fundClassification: String,
+    controllingCorporation: String,
+    totalAssets: Number,
+    inceptionDate: Date,
+    targetPopulation: String,
+    specialization: String,
+    subSpecialization: String,
+    avgAnnualManagementFee: Number,
+    avgDepositFee: Number,
+    standardDeviation: Number,
+    alpha: Number,
+    sharpeRatio: Number,
+    historicalData: [{
+      reportPeriod: Number,
+      monthlyYield: Number,
+      yearToDateYield: Number,
+      yieldTrailing3Yrs: Number,
+      yieldTrailing5Yrs: Number,
+      totalAssets: Number,
+      liquidAssetsPercent: Number,
+      stockMarketExposure: Number,
+      foreignExposure: Number,
+      foreignCurrencyExposure: Number
+    }]
+  });
 const userSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true },
     password: { type: String, required: true },
@@ -7,7 +35,9 @@ const userSchema = new mongoose.Schema({
     lastname: {type:String, require:true},
     securityQuestion: {type:String, require:true},
     securityAnswer: { type:String, require:true},
-    role: { type: String, enum: ['user', 'admin','tech'], default: 'user'}
+    role: { type: String, enum: ['user', 'admin','tech'], default: 'user'},
+    favoriteFunds: [fundAnalyticsSchema]
+
 });
 
 const User = mongoose.model('User', userSchema);
